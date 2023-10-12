cf383d74a950eb7d9849b6203f04369978eda6f3
Lucie Brackenier <lucie.brackenier@ynov.com>
2023-10-12 13:35:03 +0200

    added nodemon

---
+++ Back/config/db-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: 'the most secret key ever',
+    database: 'mongodb+srv://lionelabatan:TaskBoard123@taskboard.0fdyvlu.mongodb.net/?retryWrites=true&w=majority',
+}
---
+++ Back/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const morgan = require('morgan');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+let corsOptions = {
+    origin: 'http://localhost:3000',
+};
+
+connectDB();
+
+const app = express();
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+const port = process.env.PORT || 3000;
+
+app.use(cors(corsOptions));
+
+
+// app.use(express.json()); in this case, it was not adapted to the image format
+
+//A absolument ajouter si on veut lire les données de type JSON
+app.use(bodyParser.json({limit: '50mb', extended: true, type: 'application/json'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.text({limit: '50mb'}))
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+//Routes
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+
+//go to http://localhost:3000/api-docs/ to see the documentation when the server is running
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+app.listen(port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`));
