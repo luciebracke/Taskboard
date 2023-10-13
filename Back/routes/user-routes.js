a9d1c5aaf39c1b32829908c71a6a7ab2d1cd72af
Lucie Brackenier <lucie.brackenier@ynov.com>
2023-10-13 11:40:36 +0200

    added user basic crud

--- Back/routes/user-routes.js
+++ Back/routes/user-routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const userController = require('../controllers/user-controller');
 
 router
+.get('/', userController.getAllUsers)
 .post('/register', userController.createUser)
+.patch('/:id', userController.updateUser)
+.delete('/:id', userController.deleteUser);
 
 module.exports = router;
