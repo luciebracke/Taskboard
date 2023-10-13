c384ea703d1a7ff0125a84968b4f41d1b3276521
Lucie Brackenier <lucie.brackenier@ynov.com>
2023-10-13 14:18:27 +0200

    added swagger to the project

--- Back/routes/user-routes.js
+++ Back/routes/user-routes.js
@@ -10,3 +10,140 @@ router
 .delete('/:id', userController.deleteUser);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * 
+ * paths:
+ *   /api/users:
+ *     get:
+ *       summary: Gets all users
+ *       tags:
+ *         - Users
+ *       parameters:
+ *         - name: Authorization
+ *           in: header
+ *           description: Bearer token
+ *           required: true
+ *           type: string
+ *           format: string        
+ * 
+ *       responses:
+ *         200:
+ *           description: OK
+ *         500:
+ *          description: Internal server error
+ * 
+ *   api/user/register:
+ *     post:
+ *       tags:
+ *         - Users
+ *       summary: Register a new user
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                 password:
+ *                   type: string
+ *             example:
+ *              email: "john.doe@gmail.com"
+ *              password: "password"           
+ *          
+ *       responses:
+ *         201:
+ *           description: Created
+ *         500: 
+ *           description: Internal Server Error
+ * 
+ *   api/user/login:
+ *     post:
+ *       tags:
+ *         - Users
+ *       summary: Login a user
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                 password:
+ *                   type: string
+ *               example:
+ *                 email: "luluberlu@hotmail.fr"
+ *                 password: "password"
+ * 
+ *       responses:
+ *         200:
+ *           description: OK 
+ *         500:
+ *           description: Internal Server Error
+ * 
+ *   api/user/{id}:
+ *     patch:
+ *       tags:
+ *        - Users
+ *       summary: Modify a user by id
+ *       parameters:
+ *         - name: Authorization
+ *           in: header
+ *           description: Bearer token
+ *           required: true
+ *           type: string
+ *           format: string       
+ *         - name: id
+ *           in: path
+ *           description: id of the user
+ *           required: true
+ *           type: string
+ *           format: string
+ *       requestBody:
+ *          description: Fields that can be modified
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  email:
+ *                    type: string
+ *                  password:
+ *                    type: string               
+ *
+ *       responses:
+ *         200:
+ *           description: OK
+ *         500:
+ *           description: Internal Server Error
+ * 
+ *     delete:
+ *       tags:
+ *        - Users
+ *       summary: Delete a user by id
+ *       parameters:
+ *         - name: Authorization
+ *           in: header
+ *           description: Bearer token
+ *           required: true
+ *           type: string
+ *           format: string
+ *         - name: id
+ *           in: path
+ *           description: id of the user
+ *           required: true
+ *           type: string
+ *           format: string
+ * 
+ *       responses:
+ *         200:
+ *           description: OK
+ *         500:
+ *           description: Internal Server Error      
+ *                                                              
+ */
--- Back/server.js
+++ Back/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
 const cors = require('cors');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
+
 const userRoutes = require('./routes/user-routes');
 const boardRoutes = require('./routes/board-routes');
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const swaggerOptions = require('./config/swagger-config');
+
 let corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000'
 };
 
 connectDB();
@@ -16,7 +20,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 //A absolument ajouter si on veut lire les données de type JSON
 app.use(bodyParser.json({limit: '50mb', extended: true, type: 'application/json'}));
@@ -26,5 +30,10 @@ app.use(bodyParser.text({limit: '50mb'})) */
 //Routes
 app.use('/api/users', userRoutes);
 app.use('/api/board', boardRoutes);
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+//go to http://localhost:3000/api-docs/ to see the documentation when the server is running
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.listen(port, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`));
